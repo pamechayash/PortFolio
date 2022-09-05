2574f4f7d14f915604e70dcfdcdfb6e3e0b0dcd0
yashpamecha <yj69771@gmail.com>
2022-09-05 16:59:28 +0530

    second commit

--- src/components/about/content.jsx
+++ src/components/about/content.jsx
@@ -2,7 +2,7 @@ import TypewriterComponent from "typewriter-effect";
 import "./about.css"
 export default function Content(){
     return <div className="content"  >
-  <h4>I am Yash Pamecha,Currently enrolling in Computer Science (Specialisation in Data Science) from JECRC University, Jaipur ,India.
+  <h4>I am Yash Pamecha, enrolling in Computer Science (Specialisation in Data Science) from JECRC University, Jaipur ,India.
 
   
 
